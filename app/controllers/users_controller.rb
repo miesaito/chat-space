48ac218ad7662d3c5c4d0b73887302565c606c52
齋藤美恵 <yuunyan04@gmail.com>
2017-10-09 19:32:51 +0900

    revision,  added edit and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,13 @@
 class UsersController < ApplicationController
 
   def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
     user = User.find(params[:id])
     if user.id == current_user.id
       user.update(user_params)
-    end
   end
 
   private
   def user_params
-    params.permit(:name, :email)
+    redirect_to action: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root  'messages#index'
-  patch 'users/:id' => 'users#update'
-  get   'users/:id/edit' => 'users#edit'
-
+  resources :messages
+  resources :users
 end
