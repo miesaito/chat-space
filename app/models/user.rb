fbd0be3bb9deef79e70af9a8411d1321b7bf3f1f
齋藤美恵 <yuunyan04@gmail.com>
2017-10-10 14:11:47 +0900

    Add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, :email, presence: true
+  validates :email, uniqueness: :true
 end
