7b4e0f6d3f0c6a1bfaf25b48600805c27c22a782
齋藤美恵 <yuunyan04@gmail.com>
2017-10-12 20:14:57 +0900

    Create Group creation function

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'erb2haml'
 gem 'haml-rails'
 gem 'font-awesome-rails'
+gem 'rails-i18n'
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,14 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :messages
   has_many :members
+
+  validates :name, presence: true
+  validate :add_error_sample
+
+  def add_error_sample
+    # nameが空のときにエラーメッセージを追加する
+    if name.empty?
+    end
+  end
 end
 
