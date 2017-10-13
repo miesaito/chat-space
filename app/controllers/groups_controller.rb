f6e835c5f9e07aaa7e4adb17c6bc16c19f25a6db
齋藤美恵 <yuunyan04@gmail.com>
2017-10-13 16:07:39 +0900

    Create edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,10 @@
 class GroupsController < ApplicationController
-  def show
-    @groups = Group.find(params[:id])
-  end
-
+  before_action :set_group, only:[:edit, :update ]
   def new
     @group = Group.new
   end
 
   def create
-    # binding.pry
     @group = Group.new(group_params)
     if @group.save
       redirect_to :root, notice: "グループを作成しました"
@@ -21,10 +17,19 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to :root
+    else
+      render :edit
+    end
   end
 
 private
- def group_params
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
 
