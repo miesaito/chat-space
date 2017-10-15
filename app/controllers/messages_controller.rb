e37514c65d7baf6ae4a54533a2723722d5452a76
齋藤美恵 <yuunyan04@gmail.com>
2017-10-15 19:34:29 +0900

    Add validate to sending message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,25 @@
 class MessagesController < ApplicationController
-
   def index
-  @groups = current_user.groups
-  end
-
-  def new
+    # binding.pry
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
   def create
+    #binding.pry
+    @group = Group.find(params[:group_id])
+    @message = current_user.messages.new(message_params)
+    if @message.save
+      redirect_to action: :index
+    else
+      render :index, alert: 'メッセージを入力してください'
+    end
   end
+
+private
+  def message_params
+    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id])
+  end
+
 end
