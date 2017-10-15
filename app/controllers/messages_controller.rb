be6b549727860ec38c715900f8ec221bc81d71de
齋藤美恵 <yuunyan04@gmail.com>
2017-10-15 22:04:08 +0900

    add before action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only:[:index, :create ]
   def index
     @groups = current_user.groups
-    @group = Group.find(params[:group_id])
     @message = Message.new
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @message = current_user.messages.new(message_params)
     @groups = current_user.groups
     if @message.save
@@ -18,6 +17,10 @@ class MessagesController < ApplicationController
   end
 
 private
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
   def message_params
     params.require(:message).permit(:text, :image).merge(group_id: params[:group_id])
   end
