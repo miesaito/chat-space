d4478fd8c1b65347cde2574eee31edc83fe716c4
齋藤美恵 <yuunyan04@gmail.com>
2017-10-15 19:26:12 +0900

    Add column to message table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013081207) do
+ActiveRecord::Schema.define(version: 20171014115010) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at",               null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171013081207) do
     t.text     "image",      limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "group_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -48,6 +49,7 @@ ActiveRecord::Schema.define(version: 20171013081207) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "group_id"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
